58b3a2fc83f7999d6c8e166842d3bb69d7ef2866
pkishalov <pkishalov@fe48678a-375d-11e5-9b86-91b886df6bc0>
2016-07-18 12:15:11 +0000

    - doing display
    git-svn-id: file:///x/prj/dhelectronics/DRCXX/sw/Repos/Linux%20Host%20Application@71 fe48678a-375d-11e5-9b86-91b886df6bc0

--- C-API/peripherals/Display.cpp
+++ C-API/peripherals/Display.cpp
@@ -14,7 +14,12 @@ using namespace std;
 
 Display::Display(SpiProto &proto)
     : proto_(proto)
+    , displayFilled_(false)
+    , fillColorWhite_(false)
 {
+    // zeroing both buffers
+    memset(buffer_, 0, BUFSIZE);
+    memset(diffBuffer_, 0, BUFSIZE);
 }
 
 
@@ -24,47 +29,314 @@ RESULT Display::enable(bool on)
 }
 
 
-RESULT Display::clear()
+void Display::fill(bool white)
 {
-    Command <2> cmd(CMD_DISPLAY_CLEAR);
-    set_8(cmd.data(), 0, 0);
-    set_8(cmd.data(), 1, 0);
-    Response <0> rsp;
-    return proto_.xmit(cmd, rsp, DISPLAY_CLEAR_WAIT_MS);
+    // remember that the display was filled with color
+    displayFilled_ = true;
+    fillColorWhite_ = white;
+
+    // fill both of the buffers with the color
+    const uint8_t color = white ? 0xff : 0;
+    memset(buffer_, color, BUFSIZE);
+    memset(diffBuffer_, color, BUFSIZE);
 }
 
 
-RESULT Display::invert(bool on)
+uint8_t * Display::bufferAddress(uint8_t byteX, uint8_t y)
 {
-    Command <1> cmd(CMD_DISPLAY_INVERSION);
-    set_8(cmd.data(), 0, on ? 1 : 0);
-    Response <0> rsp;
-    return proto_.xmit(cmd, rsp);
+    return buffer_ + byteX + PITCH * y;
 }
 
 
-RESULT Display::selectFont(int)
-{   
-    // may be implemented in future
-    return RESULT_FUNCTION_INVALID;
+/**
+ * @brief isRectOnScreen
+ * @param x
+ * @param y
+ * @param w - width of the rectangle, min value is 1
+ * @param h - height of the rectangle, min value is 1
+ * @param xEnd - returned end of x, non-inclusive
+ * @param yEnd - returned end of y, non-inclusive
+ * @return - true means rect intersects with the screen
+ */
+bool Display::isRectOnScreen(int x, int y, int w, int h, int &xEnd, int &yEnd)
+{
+    if(w < 1 || h < 1)
+        return false;
+
+    if(y >= HEIGHT || x >= WIDTH)
+        return false;
+
+    xEnd = x + w;
+    if(xEnd <= 0)
+        return false;
+
+    yEnd = y + h;
+    if(yEnd <= 0)
+        return false;
+
+    return true;
 }
 
 
-RESULT Display::setPosition(int x, int y)
+void Display::columnOr(uint8_t *data, uint8_t mask, uint8_t height)
 {
-    Command <2> cmd(CMD_DISPLAY_SETPOS);
-    set_8(cmd.data(), 0, uint8_t(x));
-    set_8(cmd.data(), 1, uint8_t(y));
-    Response <0> rsp;
-    return proto_.xmit(cmd, rsp);
+    uint8_t *d = data;
+    for(uint8_t i = 0; i < height; ++i, d += Display::PITCH)
+        *d |= mask;
 }
 
 
-RESULT Display::bitmap(const unsigned char *data, int width, int height, BMP_FUNC func)
+void Display::columnXor(uint8_t *data, uint8_t mask, uint8_t height)
 {
-    assert(data);
+    uint8_t *d = data;
+    for(uint8_t i = 0; i < height; ++i, d += Display::PITCH)
+        *d ^= mask;
+}
 
 
+void Display::columnAnd(uint8_t *data, uint8_t mask, uint8_t height)
+{
+    uint8_t *d = data;
+    for(uint8_t i = 0; i < height; ++i, d += Display::PITCH)
+        *d &= mask;
+}
+
+
+void Display::columnSet(uint8_t *data, uint8_t mask, uint8_t height)
+{
+    uint8_t *d = data;
+    for(uint8_t i = 0; i < height; ++i, d += Display::PITCH)
+        *d = mask;
+}
+
+
+void Display::horizontalLine(uint8_t xStart, uint8_t xEnd, uint8_t y)
+{
+    // start of the line
+    const uint8_t xStartByte = x >> 3;       // divide by 8
+    const uint8_t bit = x & 0x07;
+    const uint8_t mask = 1 << bit;
+}
+
+
+void Display::verticalLine(uint8_t x, uint8_t yStart, uint8_t yEnd)
+{
+    const uint8_t xByte = x >> 3;       // divide by 8
+    const uint8_t mask = 1 << (x & 0x07);
+    const uint8_t height = yEnd - yStart + 1;
+
+    uint8_t *d = bufferAddress(xByte, yStart);
+    columnOr(d, mask, height);
+}
+
+
+void Display::fillRect(int x, int y, int w, int h, bool white)
+{
+    // check if the rect is outside the screen
+    int xEnd, yEnd;
+    if(!isRectOnScreen(x, y, w, h, xEnd, yEnd))
+        return;
+
+    --xEnd; // to inclusive
+
+    // clamp to the screen
+    const int xStart = x > 0 ? x : 0;
+    if(xEnd >= WIDTH)
+        xEnd = WIDTH - 1;   // inclusive
+    const uint8_t columnHeight = yEnd - yStart;
+
+    const int yStart = y > 0 ? y : 0;
+    if(yEnd > HEIGHT)
+        yEnd = HEIGHT;      // non-inclusive
+
+    // prepare for drawing
+    uint8_t xStartByte = xStart >> 3;       // divide by 8
+    uint8_t xEndByte = xEnd >> 3;
+
+    const uint8_t xStartBits = xStart & 0x07;
+    const uint8_t xEndBits = xEnd & 0x07;   // xEndBits are inclusive
+
+    uint8_t startMask = 0xFF << xStartBits;
+    uint8_t endMask = ~(0xFF << xEndBits);
+    uint8_t *d = bufferAddress(xStartByte, yStart);
+
+    if(xStartByte < xEndByte)
+    {
+        // drawing the first column
+        if(white)
+            columnOr(d, startMask, columnHeight);
+        else
+            columnAnd(d, ~startMask, columnHeight);
+
+        ++d;
+
+        // filling the intermediate columns
+        for(uint8_t xb = xStartByte + 1; xb < xEndByte; ++xb, ++d)
+            columnSet(d, white ? 0xff : 0, columnHeight);
+    }
+    else
+    { // start and end of x is on the same column
+        endMask &= startMask;    // mask is for the black color
+    }
+
+    // drawing the last (or the only) column
+    if(white)
+        columnOr(d, endMask, columnHeight);
+    else
+        columnAnd(d, ~endMask, columnHeight);
+}
+
+
+void Display::drawRect(int x, int y, int w, int h, bool white)
+{
+    // check if the rect is outside the screen
+    int xEnd, yEnd;
+    if(!isRectOnScreen(x, y, w, h, xEnd, yEnd))
+        return;
+
+    --xEnd; --yEnd; // to include the endpoints
+
+    // clamp to the screen
+    bool drawLeft;
+    int xStart;
+    if(x >= 0)
+    {
+        xStart = x;
+        drawLeft = true;
+    }
+    else
+    {
+        xStart = 0;
+        drawLeft = false;
+    }
+
+    bool drawRight;
+    if(xEnd < WIDTH)
+    {
+        drawRight = true;
+    }
+    else
+    {
+        xEnd = WIDTH - 1;   // inclusive
+        drawRight = false;
+    }
+
+    const uint8_t columnHeight = yEnd - yStart;
+
+    const int yStart;
+    bool drawTop;
+    if(y >= 0)
+    {
+       yStart = y;
+       drawTop = true;
+    }
+    else
+    {
+        yStart = 0;
+        drawTop = false;
+    }
+
+    bool drawBottom;
+    if(yEnd < HEIGHT)
+    {
+        drawBottom = true;
+    }
+    else
+    {
+        yEnd = HEIGHT - 1;
+        drawBottom = false;
+    }
+
+    // drawing
+    if(drawTop)
+        horizontalLine(xStart, xEnd, yStart);
+
+    if(drawBottom)
+        horizontalLine(xStart, xEnd, yEnd);
+
+    if(drawLeft)
+        verticalLine(xStart, yStart, yEnd);
+
+    if(drawRight)
+        verticalLine(xEnd, yStart, yEnd);
+}
+
+
+void Display::invertRect(int x, int y, int w, int h)
+{
+    // check if the rect is outside the screen
+    int xEnd, yEnd;
+    if(!isRectOnScreen(x, y, w, h, xEnd, yEnd))
+        return;
+
+    --xEnd; // to inclusive
+
+    // clamp to the screen
+    const int xStart = x > 0 ? x : 0;
+    if(xEnd >= WIDTH)
+        xEnd = WIDTH - 1;   // inclusive
+    const uint8_t columnHeight = yEnd - yStart;
+
+    const int yStart = y > 0 ? y : 0;
+    if(yEnd > HEIGHT)
+        yEnd = HEIGHT;      // non-inclusive
+
+    // prepare for drawing
+    uint8_t xStartByte = xStart >> 3;       // divide by 8
+    uint8_t xEndByte = xEnd >> 3;
+
+    const uint8_t xStartBits = xStart & 0x07;
+    const uint8_t xEndBits = xEnd & 0x07;   // xEndBits are inclusive
+
+    uint8_t startMask = 0xFF << xStartBits;
+    uint8_t endMask = ~(0xFF << xEndBits);
+    uint8_t *d = bufferAddress(xStartByte, yStart);
+
+    if(xStartByte < xEndByte)
+    {
+        // drawing the first column
+        columnXor(d, startMask, columnHeight);
+
+        ++d;
+
+        // filling the intermediate columns
+        for(uint8_t xb = xStartByte + 1; xb < xEndByte; ++xb, ++d)
+            columnXor(d, 0xff, columnHeight);
+    }
+    else
+    { // start and end of x is on the same column
+        endMask &= startMask;    // mask is for the black color
+    }
+
+    // drawing the last (or the only) column
+    columnXor(d, endMask, columnHeight);
+}
+
+
+void Display::bitmap(int x, int y, const Bitmap &bmp)
+{
+}
+
+
+void Display::bitmap(int x, int y, const FT_Bitmap &bmp)
+{
+}
+
+
+RESULT Display::flush()
+{
+
+
+    if(displayFilled_)
+    {
+
+        displayFilled_ = false;
+    }
     return RESULT_OK;
 }
 
+
+RESULT Display::swap()
+{
+    return RESULT_OK;
+}
--- C-API/peripherals/Display.h
+++ C-API/peripherals/Display.h
@@ -16,36 +16,48 @@ class SpiProto;
 class Display : public IDisplay
 {
 public:
+    static const int WIDTH = 128;
+    static const int HEIGHT = 64;
+
     static const uint16_t DISPLAY_CLEAR_WAIT_MS    = 2;
     static const uint16_t DISPLAY_SYMBOL_WAIT_MS   = 1;
     static const uint16_t DISPLAY_SHOW_WAIT_MS     = 12; // 9, 10
 
     Display(SpiProto &proto);
 
-    virtual RESULT enable(bool on);
-    virtual RESULT fill(bool white);
-    virtual RESULT fillRect(int x, int y, int w, int h, bool white);
-    virtual RESULT drawRect(int x, int y, int w, int h, bool white);
-    virtual RESULT invertRect(int x, int y, int w, int h);
+    virtual RESULT  enable(bool on);
+    virtual void    fill(bool white);
+    virtual void    fillRect(int x, int y, int w, int h, bool white);
+    virtual void    drawRect(int x, int y, int w, int h, bool white);
+    virtual void    invertRect(int x, int y, int w, int h);
 
-    virtual RESULT bitmap(int x, int y, const Bitmap &bmp);
-    virtual RESULT bitmap(int x, int y, const FT_Bitmap &bmp);
+    virtual void    bitmap(int x, int y, const Bitmap &bmp);
+    virtual void    bitmap(int x, int y, const FT_Bitmap &bmp);
 
     virtual RESULT flush();
     virtual RESULT swap();
 
 private:
-    static const int WIDTH = 128;
-    static const int HEIGHT = 64;
-    static const int BUFSIZE = WIDTH * HEIGHT / 8;
+    static const int PITCH = WIDTH / 8;
+    static const int BUFSIZE = PITCH * HEIGHT;
 
-    SpiProto & proto_;
-    uint8_t     buffers_[2][BUFSIZE];           // two buffers
-    uint8_t     compressedDiff_[BUFSIZE + 3];   // compressed diff
+    uint8_t *   bufferAddress(uint8_t byteX, uint8_t y);
+    static bool isRectOnScreen(int x, int y, int w, int h, int &xEnd, int &yEnd);
+    static void columnOr(uint8_t *data, uint8_t mask, uint8_t height);
+    static void columnXor(uint8_t *data, uint8_t mask, uint8_t height);
+    static void columnAnd(uint8_t *data, uint8_t mask, uint8_t height);
+    static void columnSet(uint8_t *data, uint8_t mask, uint8_t height);
+    void horizontalLine(uint8_t xStart, uint8_t xEnd, uint8_t y);
+    void verticalLine(uint8_t x, uint8_t yStart, uint8_t yEnd);
+
+    SpiProto &  proto_;
+    uint8_t     buffer_[BUFSIZE];               // main buffer
+    uint8_t     diffBuffer_[BUFSIZE];           // buffer used for diff
+    uint8_t     compressedDiff_[BUFSIZE + 3];   // compressed diff of the two buffers
     uint16_t    compressedSize_;
     uint16_t    sentByteIdx_;
-    uint8_t     currentBufferIdx_;              // current buffer index
-    uint8_t *   currentBuffer_;                // pointer to current buffer
+    bool        displayFilled_;
+    bool        fillColorWhite_;
 };
 
 
