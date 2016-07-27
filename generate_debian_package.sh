aa369c4f38d8201448d1cfac3fd069e0f482a375
pkishalov <pkishalov@fe48678a-375d-11e5-9b86-91b886df6bc0>
2016-07-27 12:49:45 +0000

    - new debian package, no json server
    git-svn-id: file:///x/prj/dhelectronics/DRCXX/sw/Repos/Linux%20Host%20Application@88 fe48678a-375d-11e5-9b86-91b886df6bc0

--- generate_debian_package.sh
+++ generate_debian_package.sh
@@ -9,37 +9,17 @@ if [[ $3 != "armel" && $3 != "armhf" ]] ; then
 	exit -1
 fi
 
-if [[ $4 != "systemd" && $4 != "init.d" ]] ; then
-	echo ERROR: unknown init manager $4, must be systemd|init.d 
-	exit -1
-fi
-
 TMP=/tmp/drc-01
 svn export --force $1/DEBIAN_PACKAGE $TMP
-mkdir -p $TMP/usr/lib $TMP/root
+mkdir -p $TMP/usr/lib
 
 cd $2
 cp C-API/libc-api.so.1 DHCOM_HAL/libDHCOM_HAL.so.1 $TMP/usr/lib
-cp JSON-API/json-server $TMP/root
 cd -
 
 cd $TMP/DEBIAN
 sed "s/Architecture:.*/Architecture:$3/" <control >control.edited
 mv control.edited control
-
-if [[ $4 == "systemd" ]] ; then
-	mv preinst.systemd preinst
-	mv postinst.systemd postinst
-	rm *.update-rc
-	cd - ; cd $TMP/etc
-	rm -rf init.d
-else
-	mv preinst.update-rc preinst
-	mv postinst.update-rc postinst
-	rm *.systemd
-	cd - ; cd $TMP/etc
-	rm -rf systemd 
-fi
 cd -
 
 dpkg -b $TMP $2/drc-01.deb
