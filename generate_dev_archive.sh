6c580b8c7fba86d1a0c25bcccad39adc09bdbf3f
Ludwig Zenz <lzenz@dh-electronics.com>
2019-10-10 16:36:42 +0200

    PSoC Host Application: repair dev build script

--- generate_dev_archive.sh
+++ generate_dev_archive.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-# $1 - OUT_PWD $2 - DHCOM_HAL
+# $1 - SRC_DIR
+# $2 - BUILD_DIR
 
 if [ $# -ne 2 ]; then
-	echo two args must be supplied - OUT_PWD and DHCOM_HAL
+	echo two args must be supplied - SRC_DIR BUILD_DIR
 	exit 1
 fi
 
-PACKAGE=drc02-dev
+PACKAGE=psoc-capi-drc02-dev
 OUT_DIR=/var/tmp/$PACKAGE
 rm -rf $OUT_DIR
 mkdir -p $OUT_DIR/lib $OUT_DIR/bin $OUT_DIR/src 
@@ -14,9 +15,9 @@ mkdir -p $OUT_DIR/lib $OUT_DIR/bin $OUT_DIR/src
 cp -R $1/C-API/api/include $OUT_DIR/include && \
 cp -R $1/Tests $OUT_DIR/src/Tests && \
 cp -R $1/Examples $OUT_DIR/src/Examples && \
-cp -P $1/C-API/libc-api.so* $2/libDHCOM_HAL.so* $OUT_DIR/lib && \
-cp -P $1/Tests/tests $OUT_DIR/bin && \
-cp -P $1/Examples/example $OUT_DIR/bin 
+cp -P $2/C-API/libc-api.so* $2/DHCOM_HAL/libDHCOM_HAL.so* $OUT_DIR/lib && \
+cp -P $2/Tests/tests $OUT_DIR/bin && \
+cp -P $2/Examples/example $OUT_DIR/bin 
 
 PLACE=$PWD
 cd /var/tmp
