76e8c506179729246d49fb63cac0f1198e9fdb5c
Ludwig Zenz <lzenz@dh-electronics.com>
2019-10-10 11:55:07 +0200

    PSoC Host Application: Version 9.2

--- C-API/api/include/c-api/stats.h
+++ C-API/api/include/c-api/stats.h
@@ -23,7 +23,7 @@ extern "C" {
 #define API_VERSION     9
 
 /** software revision of C-API library */
-#define API_REVISION    1
+#define API_REVISION    2
 
 /**
  * \brief Get count of checksum errors of SPI command detected by MCU
