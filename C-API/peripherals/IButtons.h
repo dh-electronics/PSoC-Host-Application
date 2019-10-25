2511ff987eff3270852527934bb49aae956e57c2
Dominik Kierner <dkierner@dh-electronics.com>
2019-10-25 10:10:23 +0200

    Add virtual destructor to IButtons.h

--- C-API/peripherals/IButtons.h
+++ C-API/peripherals/IButtons.h
@@ -11,6 +11,7 @@ namespace drc01
 class IButtons
 {
 public:
+    virtual ~IButtons(){}
     virtual void   setCallback(buttonsCallback *callback)   =0;
     virtual RESULT handleEvents()                           =0;
     virtual bool   getState(enum BUTTON button) const       =0;
