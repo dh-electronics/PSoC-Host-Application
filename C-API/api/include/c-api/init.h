d240e9b674652a490a1874349927672cbdcd6c52
Ludwig Zenz <lzenz@dh-electronics.com>
2020-01-07 13:43:46 +0100

    PSoC Host Application: Update C-API open/close/idle documentation

--- C-API/api/include/c-api/init.h
+++ C-API/api/include/c-api/init.h
@@ -5,10 +5,12 @@
  *  \brief API initialization and termination
  *
  * Before the Linux operating system boots and the Cypress MCU is accessed by
- * the user application, Green RUN LED will flash with 800mS interval. After the first
- * access, C-API takes over the flashing and flashes with 300mS interval.
- * This ends when user application opens API - Green LED will stop flashing and
+ * the user application, Green RUN LED will flash with 800ms interval. This ends
+ * when user application opens API: Green LED will stop flashing and the
  * control over the Led is transferred to the application.
+ *
+ * After the idleAPI call, C-API takes over the flashing and flashes with
+ * 300ms interval.
  *
  * Note: Green RUN LED will stop flashing when first API call is executed.
  * The user application software must then explicitly control the Green RUN LED.
@@ -36,7 +38,9 @@ enum RESULT openApi();
 /**
  * \brief close API
  *
- * brings Cypress MCU back to wait mode, cleans up resources
+ * Brings Cypress MCU back to wait mode, cleans up resources. The RUN LED will
+ * start flashing with 800ms interval, again. The pcap touch button scan
+ * is disabled.
  *
  * \return result code of requested operation
  */
@@ -45,7 +49,7 @@ enum RESULT closeApi();
 /**
  * \brief idle API
  *
- * RUN LED will start flashing with 300mS interval again. The pcap touch button
+ * RUN LED will start flashing with 300mS interval. The pcap touch button
  * scan is disabled during idle mode. (Just don't use idleApi() it if pcap touch
  * buttons should always be active.)
  *
