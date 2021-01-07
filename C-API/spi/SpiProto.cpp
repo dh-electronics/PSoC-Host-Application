9f28c8ab129e7642712c7e0dca0e4d37444a6dd5
Ludwig Zenz <lzenz@dh-electronics.com>
2021-01-07 15:43:43 +0100

    C-API: Workaround for blocking pthread_join of heartbeat thread

--- C-API/spi/SpiProto.cpp
+++ C-API/spi/SpiProto.cpp
@@ -118,8 +118,28 @@ bool SpiProto::start()
 
 void SpiProto::stop()
 {
+    int status;
+    struct timespec timeout;
+
+    if (clock_gettime(CLOCK_REALTIME, &timeout) == -1)
+	syslog(LOG_ERR, "Failed to get clock time!");
+
+    timeout.tv_sec += 2;
+
     running_ = false;
-    pthread_join(pthread_, NULL);
+
+    /* previously pthread_join was used here. Very rarely pthread_join
+     * was blocking.
+     *
+     * The reason could not be found in a intense debugging session.
+     * TODOs:
+     *  - Investigate the usage of waitCondition_
+     */
+    status = pthread_timedjoin_np(pthread_, NULL, &timeout);
+    if (status && status != ESRCH) {
+	syslog(LOG_WARNING, "Failed to join heartbeat thread! status: %s", strerror(status));
+	pthread_cancel(pthread_);
+    }
 }
 
 
@@ -380,8 +400,10 @@ void SpiProto::run()
                 if(pingResult)
                 {
                     PIC_MODE mode = readMode(&res);
-                    if(RESULT_OK == res && mode != PIC_MODE_ACTIVE)
+                    if(RESULT_OK == res && mode != PIC_MODE_ACTIVE && isRunning())
+                    {
                         ApiManager::self()->stateRestore();
+                    }
                 }
                 else
                 {
