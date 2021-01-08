24c3ff1b36ddc13f35f01a58300ca11a6c198f52
Ludwig Zenz <lzenz@dh-electronics.com>
2021-01-08 08:45:57 +0100

    Version 9.11

--- C-API/api/include/c-api/stats.h
+++ C-API/api/include/c-api/stats.h
@@ -24,7 +24,7 @@ extern "C" {
 #define API_VERSION     9
 
 /** software revision of C-API library */
-#define API_REVISION    10
+#define API_REVISION    11
 
 /**
  * \brief Get count of checksum errors of SPI command detected by MCU
